0a464cf15942fd0b7aa176b4d23cbfd3dba53046
Kay Kim <kimjh@bawi.org>
2021-06-08 20:19:13 +0900

    change App-test to Feed-test

--- __tests__/Feed-test.tsx
+++ __tests__/Feed-test.tsx
@@ -1,14 +1,10 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../src/screens/Home';
+import Feed from '../src/screens/main/Feed';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<Feed componentId="" />);
 });
